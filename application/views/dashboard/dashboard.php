5ad5b3fe489d5626b0dfecd7a419a80bcee57699
PRADIP2105 <pkmalnkiya@gmail.com>
2025-02-16 13:44:39 +0530

    dahsboard changes

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -2,6 +2,7 @@
 
 <div class="container d-flex justify-content-center align-items-center flex-grow-1">
     <h2>Welcome to the Dashboard!</h2>
+    <p>You are logged in as <strong><?php echo $this->session->userdata('email'); ?></strong></p>
 </div>
 
 <?php $this->load->view('templates/footer'); ?>
